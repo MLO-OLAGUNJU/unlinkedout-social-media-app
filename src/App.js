eddf905ed6e9df0d972aa44e42d577720d5c38b8
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-01-26 22:22:08 +0100

    I updated the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { Fragment } from "react";
 import Login from "./components/Login";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -8,6 +11,15 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
+          <Route
+            path="/home"
+            element={
+              <React.Fragment>
+                <Header />
+                <Home />
+              </React.Fragment>
+            }
+          />
         </Routes>
       </Router>
     </div>
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const Home = (props) => {
+  return <div>Home</div>;
+};
+
+export default Home;
