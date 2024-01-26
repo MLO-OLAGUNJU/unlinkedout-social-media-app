184a087d9000b2c631f02feeb9e01b6cc9b7cdd4
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-01-26 15:57:20 +0100

    I modified index.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Let's build the Linkedin clone</h2>
+    </div>
+  );
 }
 export default App;
